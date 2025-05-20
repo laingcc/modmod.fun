dbe0842bc7cd10ceeca51c9eaff5ddb2ba4f9376
chuck <laingcc@gmail.com>
2025-05-19 22:07:44 -0600

    feat: dark mode styling, some sfx for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
 
@@ -7,8 +8,22 @@ import {HeaderComponent} from "./components/header/header.component";
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'fever';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.playNavigationSound();
+      }
+    });
+  }
+
+  private playNavigationSound(): void {
+    const audio = new Audio('assets/terminal_click.wav');
+    audio.play();
+  }
 }
+
