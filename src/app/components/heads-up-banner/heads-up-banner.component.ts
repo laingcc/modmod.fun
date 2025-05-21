4cacff3d83fc4716a00def0df2ee12355f8e8fd8
chuck <laingcc@gmail.com>
2025-05-20 23:43:00 -0600

    Update HeadsUpBanner component content and add RouterLink
    Updated the banner text to be more concise and user-focused, emphasizing simplicity and anonymity. Integrated Angular's RouterLink for internal navigation to the tripcodes page.

--- src/app/components/heads-up-banner/heads-up-banner.component.ts
+++ src/app/components/heads-up-banner/heads-up-banner.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-heads-up-banner',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './heads-up-banner.component.html',
   styleUrl: './heads-up-banner.component.scss'
 })
