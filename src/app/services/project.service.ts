dcf46c238189336c8c6901ee901d227edb7fa097
unixcrush <claire@quadratrix.ca>
2025-05-22 00:11:08 -0600

    Add image gallery modal for dashboard with thumbnail previews
    Introduced an image gallery modal component to enhance the user experience when viewing project images on the dashboard. Users can now interact with a gallery view, navigate between images, and select thumbnails for a better preview experience. Updated related services and styles to support the new feature.

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -16,36 +16,6 @@ export type Project = {
 
 @Injectable()
 export class ProjectService {
-  // getAllProjects(): Project[]{
-  //   return [...Array(Math.floor(Math.random()*20))].map(v => this.getMockProject())
-  //
-  // }
-  //
-  //
-  // getMockProject(): Project {
-  //   const lorem = new LoremIpsum(
-  //     {
-  //       sentencesPerParagraph: {
-  //         max: 8,
-  //         min: 4
-  //       },
-  //       wordsPerSentence:{
-  //         max:16,
-  //         min:1
-  //       }
-  //     }
-  //   )
-  //
-  //   return {
-  //     id: Math.floor(Math.random()*9999),
-  //     name: lorem.generateWords(Math.floor(Math.random()*10)),
-  //     description: lorem.generateParagraphs(25),
-  //     author: lorem.generateWords(2).split(' ').join(''),
-  //
-  //   }
-  //
-
-  // }
 getAllProjects(): Observable<Project[]> {
   return this.http.get<Project[]>(`${this.envService.apiHost}/threads`);
 }
